8987494c8476b95e12ae8d264bebdb28e337324a
Vikash Saini <vsai12@sapient.com>
2018-04-23 11:12:38 +0530

    Signed-off-by: Vikash Saini <vsai12@sapient.com>

--- src/main/java/com/tasks/TasksApplication.java
+++ src/main/java/com/tasks/TasksApplication.java
@@ -1,10 +1,64 @@
 package com.tasks;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tasks.entity.Task;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RequestMapping("/tasks")
 public class TasksApplication {
+
+	private List<Task> tasksList = new ArrayList<Task>();
+
+	@PostConstruct
+	void loadTasks() {
+		tasksList.add(new Task("1", "High", "Get Car", "Get your car delivered from Rajdhani VW"));
+		tasksList.add(new Task("2", "High", "RC and RTO done", "Get RC and RTO done from local rewari secretariet."));
+		tasksList.add(new Task("3", "Low", "Music system",
+				"Geta good sound quality music system installed in your car, It must be bluetooth enabled."));
+	}
+
+	@GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Task>> findAllTasks() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccessControlAllowOrigin("*");
+		ResponseEntity<List<Task>> responseEntity = new ResponseEntity<List<Task>>(tasksList, headers, HttpStatus.OK);
+		return responseEntity;
+	}
+
+	@PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> addTask(@RequestParam String toDo, @RequestParam(required = false) String toDoDetail,
+			@RequestParam(required = false, defaultValue="Medium") String priority) {
+		tasksList.add(new Task("4", priority, toDo, toDoDetail));
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccessControlAllowOrigin("*");
+		ResponseEntity<Object> responseEntity = new ResponseEntity<>(headers, HttpStatus.CREATED);
+		return responseEntity;
+	}
+
+	@GetMapping(path = "/{taskId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Task findTask(@PathVariable String taskId) {
+		return tasksList.stream().filter(t -> t.getTaskId().equals(taskId)).findFirst().orElse(null);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
