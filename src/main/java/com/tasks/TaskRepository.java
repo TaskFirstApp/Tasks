be5438e53bb5fc1213ab8fc94dcf5529dff73516
vikashsaini01 <vikashsaini01@gmail.com>
2022-01-12 17:25:07 +0530

    moved tasks to sub folder

--- src/main/java/com/tasks/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tasks;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.tasks.entity.Task;
-
-public interface TaskRepository extends MongoRepository<Task, String>{
-	
-}
